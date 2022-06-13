9cc2ee89cc5b784dcb2a66347072b707c3eaaea8
LOUKHNATIZouhair <lakzouhair98@gmail.com>
2022-06-13 17:27:01 +0100

    Reclamation Part

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {NavbarComponent} from "../modules/navbar/navbar.component";
+import { SidebarComponent } from '../modules/sidebar/sidebar.component';
+import { DashboardComponent } from '../modules/dashboard/dashboard.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SidebarComponent,
+    DashboardComponent,
+
   ],
   imports: [
     BrowserModule,
